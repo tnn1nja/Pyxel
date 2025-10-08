0536a0913f5dc221a20c9f9954b488ae9bb924ca
tnn1nja <thenonsenseninja@gmail.com>
2025-10-08 16:47:41 +0100

    fully functional, vertices now scale with radius to keep circle filled in

--- AsciiCircle.py
+++ AsciiCircle.py
@@ -1,26 +1,23 @@
 from math import sqrt
 
-def displayGrid(grid):
+def display(grid):
     for row in grid:
         for element in row:
             print(element, end=" ")
         print("")
 
-def intRound(num):
-    return int(round(num))
+def create(radius):
+    gridSize = (radius*2) + 1
+    grid = [["."]*gridSize for x in range(gridSize)]
 
-radius = 7
-gridSize = (radius*2) + 1
-grid = [["."]*gridSize for x in range(gridSize)]
+    verts = radius*10
+    for i in range(verts+1):
+        x = (i/verts) * radius
+        y = sqrt(radius**2 - x**2)
+        for dy in (-1, 1):
+            for dx in (-1, 1):
+                grid[radius + dy*int(round(y))][radius + dx*int(round(x))] = "#"
+    
+    return grid
 
-verts = 90
-for i in range(verts+1):
-    x = (i/verts) * radius
-    y = sqrt(radius**2 - x**2)
-    grid[radius - intRound(y)][radius - intRound(x)] = "#"
-    grid[radius - intRound(y)][radius + intRound(x)] = "#"
-    grid[radius + intRound(y)][radius - intRound(x)] = "#"
-    grid[radius + intRound(y)][radius + intRound(x)] = "#"
-
-displayGrid(grid)
-
+display(create(5))
