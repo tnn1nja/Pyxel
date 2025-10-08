c2cd2dfdc353ea136da332df414e1cd99f3f480f
tnn1nja <thenonsenseninja@gmail.com>
2025-10-08 17:10:26 +0100

    circle now displays with nicer symbols

--- AsciiCircle.py
+++ AsciiCircle.py
@@ -1,14 +1,16 @@
 from math import sqrt
 
+block = "█"
+
 def display(grid):
     for row in grid:
         for element in row:
-            print(element, end=" ")
+            print(element, end="")
         print("")
 
 def create(radius):
     gridSize = (radius*2) + 1
-    grid = [["."]*gridSize for x in range(gridSize)]
+    grid = [[" "]*gridSize for x in range(gridSize)]
 
     verts = radius*10
     for i in range(verts+1):
@@ -16,8 +18,17 @@ def create(radius):
         y = sqrt(radius**2 - x**2)
         for dy in (-1, 1):
             for dx in (-1, 1):
-                grid[radius + dy*int(round(y))][radius + dx*int(round(x))] = "#"
-    
+                yCoord = radius + dy*int(round(y))
+                xCoord = radius + dx*int(round(x))
+                grid[yCoord][xCoord] = block
+
+    for row in grid:
+        for i in range(len(row)):
+            symbol = " "
+            if row[i*2] == block:
+                symbol = block
+            row.insert(i*2+1, symbol)
+
     return grid
 
 display(create(5))
