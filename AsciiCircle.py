d2ee9299d6d31275fc1e279779826a92765bd008
tnn1nja <thenonsenseninja@gmail.com>
2025-10-08 16:24:25 +0100

    Now draws circles

--- AsciiCircle.py
+++ AsciiCircle.py
@@ -9,7 +9,7 @@ def displayGrid(grid):
 def intRound(num):
     return int(round(num))
 
-radius = 5
+radius = 7
 gridSize = (radius*2) + 1
 grid = [["."]*gridSize for x in range(gridSize)]
 
@@ -17,7 +17,10 @@ verts = 90
 for i in range(verts+1):
     x = (i/verts) * radius
     y = sqrt(radius**2 - x**2)
-    grid[intRound(y)][intRound(x)] = "#" 
+    grid[radius - intRound(y)][radius - intRound(x)] = "#"
+    grid[radius - intRound(y)][radius + intRound(x)] = "#"
+    grid[radius + intRound(y)][radius - intRound(x)] = "#"
+    grid[radius + intRound(y)][radius + intRound(x)] = "#"
 
 displayGrid(grid)
 
