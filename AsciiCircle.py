41415eab15f1ffe7630f56685d3b76b57d32b346
tnn1nja <thenonsenseninja@gmail.com>
2025-10-08 23:03:47 +0100

    simplified stretching logic

--- AsciiCircle.py
+++ AsciiCircle.py
@@ -21,20 +21,13 @@ for i in range(verts+1):
             xCoord = radius + dx*int(round(x))
             grid[yCoord][xCoord] = block
 
-#Stretch
-if fill:
-    for row in grid:
-        for i in range(len(row)):
-            symbol = row[i*2]
-            row.insert(i*2, symbol)
-    spacer = ""
-else:
-    spacer = " "
-
 #Output 
 print("")
 for row in grid:
     for element in row:
-        print(element, end=spacer)
+        if fill:
+            print(element*2, end="")
+        else:
+            print(element, end=" ")
     print("")
 print("")
