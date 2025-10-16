0ccd1ee9d4bd293709ee4d52b3d970557d28693f
tnn1nja <thenonsenseninja@gmail.com>
2025-10-16 10:32:22 +0100

    changed naming convention

--- Pyxelart.py
+++ Pyxelart.py
@@ -2,48 +2,52 @@ import math
 debug = False
 block, space = ("#", ".") if debug else ("█", " ")
 
-def getGrid(width, height):
+def get_panel(width, height):
     return [[space]*width for x in range(height)]  
 
-def drawGrid(grid):
+def display(panel):
     print("")
-    for row in reversed(grid):
+    for row in reversed(panel):
         for element in row:
             if debug: print(element, end=" ")
             else: print(element*2, end="")
         print("")
     print("")
 
-def addLine(grid, a, b, c, d):
+def draw(panel, x, y):
+    pass
+
+def draw_line(panel, a, b, c, d):
     if c == a:
-        return 
+        return
     else:
         m = (b-d)/(a-c)
         iterations = round((abs(c-a)+1)*(abs(m)+1)*100)
         for i in range(iterations+1):
             x = min(a,c) + (i/iterations)*abs(c-a)
             y = m*x - m*a + b
-            grid[round(y)][round(x)] = block
+            panel[round(y)][round(x)] = block
 
-def drawCircle(radius):
+def display_circle(radius):
     resolution = radius*100
 
-    grid = getGrid(radius*2+1, radius*2+1)
+    panel = get_panel(radius*2+1, radius*2+1)
     for i in range(-resolution, resolution):
         x = radius * math.sin((i/resolution)*2*math.pi) + radius
         y = radius * math.cos((i/resolution)*2*math.pi) + radius
-        grid[round(y)][round(x)] = block
-    drawGrid(grid)
+        panel[round(y)][round(x)] = block
+    display(panel)
 
-def drawTriangle(width, height=None):
+def displayTriangle(width, height=None):
     if height == None:
         height = width
 
-    grid = getGrid(width, height)
-    addLine(grid, 0, 0, math.floor((width-1)/2), height-1)
-    addLine(grid, math.ceil(width-1)/2, height-1, width-1, 0)
-    addLine(grid, width-1, 0, 0, 0)
-    drawGrid(grid)
+    panel = get_panel(width, height)
+    draw_line(panel, 0, 0, math.floor((width-1)/2), height-1)
+    draw_line(panel, math.ceil(width-1)/2, height-1, width-1, 0)
+    draw_line(panel, width-1, 0, 0, 0)
+    display(panel)
+
 
 if __name__ == "__main__":
-    drawCircle(10)
+    ()
