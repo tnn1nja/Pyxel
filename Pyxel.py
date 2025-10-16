987aded663b90b5e3584ad90650e7462ba7e019e
tnn1nja <thenonsenseninja@gmail.com>
2025-10-16 18:42:42 +0100

    further refactored and make screen auto clear when non-debug

--- Pyxel.py
+++ Pyxel.py
@@ -1,4 +1,8 @@
 import math
+import os
+
+def clear_console():
+    os.system("cls" if os.name == "nt" else "clear")
 
 def polar_to_cartesian(radius, angle):
     x = radius + radius*math.sin(angle)
@@ -6,30 +10,30 @@ def polar_to_cartesian(radius, angle):
     return x, y
 
 class Panel:
-    stretch = True
-    block = "█"
-    space = " "
-    panel = []
-
     def __init__(self, width, height=None, debug=False):
         if height == None:
             height = width
+
+        self.debug = debug
         if debug:
             self.block = "#"
             self.space = "."
-            self.stretch = False
+        else:
+            self.block = "\u2588"
+            self.space = " "
+
         self.panel = [[self.space]*width for x in range(height)]
 
     def display(self):
-        print("")
-        for row in reversed():
+        if not self.debug:
+            clear_console()
+        for row in reversed(self.panel):
             for pixel in row:
-                if self.stretch:
+                if self.debug:
                     print(pixel, end=" ")
                 else:
                     print(pixel*2, end="")
             print("")
-        print("")
 
     def draw(self, x, y):
         if x % 1 == 0.5 or y % 1 == 0.5:
@@ -43,35 +47,40 @@ class Panel:
     def draw_line(self, a, b, c, d):
         if round(a,5) == round(c,5):
             for y in range(round(min(b,d)), round(max(b,d))+1):
-                self.draw(self.panel, a, y)
+                self.draw(a, y)
         else:
             m = (b-d)/(a-c)
             iterations = round((abs(c-a)+1)*len(self.panel))
             for i in range(iterations+1):
                 x = min(a,c) + (i/iterations)*abs(c-a)
                 y = m*x - m*a + b
-                self.draw(self.panel, x , y)
+                self.draw(x , y)
 
-    def draw_triangle(self, width, height=None, a=0, b=0):
+    def draw_triangle(self, a, b, width, height=None):
         if height == None:
             height = width
-        self.draw_line(self.panel, a, b, math.floor((width-1)/2)+a, height-1+b)
-        self.draw_line(self.panel, math.ceil(width-1)/2+a, height-1+b, width-1, b)
-        self.draw_line(self.panel, width-1+a, b, a, b)
+        self.draw_line(a, b, math.floor((width-1)/2)+a, height-1+b)
+        self.draw_line(math.ceil(width-1)/2+a, height-1+b, width-1, b)
+        self.draw_line(width-1+a, b, a, b)
 
-    def draw_circle(self, radius, a=0, b=0):
+    def draw_circle(self, a, b, radius):
         resolution = radius*100
         for i in range(resolution):
             x, y = polar_to_cartesian(radius, (i/resolution)*2*math.pi)
-            self.draw(self.panel, x+a, y+b)
+            self.draw(x+a, y+b)
 
-    def draw_ngon(self, n, radius, a=0, b=0):
+    def draw_ngon(self, a, b, n, radius):
         for i in range(n+1):
             x, y = polar_to_cartesian(radius, (i/n)*2*math.pi)
             if i > 0:
-                self.draw_line(self.panel, lx+a, ly+b, x+a, y+b)
+                self.draw_line(lx+a, ly+b, x+a, y+b)
             lx, ly = x, y
 
 
 if __name__ == "__main__":
+    my_panel = Panel(201)
+    my_panel.draw_circle(0, 0, 100)
+    my_panel.draw_ngon(10, 10, 5, 90)
+    my_panel.draw_line(30, 30, 170, 170)
+    my_panel.display()
     pass
