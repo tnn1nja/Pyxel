09a0a5ea8f76afcd66f328f542a3c6b5cd2065e5
tnn1nja <thenonsenseninja@gmail.com>
2025-10-16 10:46:31 +0100

    refactored drawing to the screen to ignore invalid values

--- Pyxelart.py
+++ Pyxelart.py
@@ -1,11 +1,14 @@
 import math
-debug = False
+debug = True
 block, space = ("#", ".") if debug else ("█", " ")
 
-def get_panel(width, height):
-    return [[space]*width for x in range(height)]  
+def get_panel(width, height=None):
+    if height == None:
+        height = width
 
-def display(panel):
+    return [[space]*width for x in range(height)]
+
+def display_panel(panel):
     print("")
     for row in reversed(panel):
         for element in row:
@@ -15,7 +18,11 @@ def display(panel):
     print("")
 
 def draw(panel, x, y):
-    pass
+    if x < len(panel) and x >= 0 and y < len(panel[0]) and y >= 0:
+        panel[round(y)][round(x)] = block
+        print(f"success: {round(x)},{round(y)}")
+    else:
+        print("fail")
 
 def draw_line(panel, a, b, c, d):
     if c == a:
@@ -26,19 +33,18 @@ def draw_line(panel, a, b, c, d):
         for i in range(iterations+1):
             x = min(a,c) + (i/iterations)*abs(c-a)
             y = m*x - m*a + b
-            panel[round(y)][round(x)] = block
+            draw(panel, x , y)
 
 def display_circle(radius):
     resolution = radius*100
-
-    panel = get_panel(radius*2+1, radius*2+1)
+    panel = get_panel(radius*2+1)
     for i in range(-resolution, resolution):
         x = radius * math.sin((i/resolution)*2*math.pi) + radius
         y = radius * math.cos((i/resolution)*2*math.pi) + radius
-        panel[round(y)][round(x)] = block
-    display(panel)
+        draw(panel, x, y)
+    display_panel(panel)
 
-def displayTriangle(width, height=None):
+def display_triangle(width, height=None):
     if height == None:
         height = width
 
@@ -46,8 +52,10 @@ def displayTriangle(width, height=None):
     draw_line(panel, 0, 0, math.floor((width-1)/2), height-1)
     draw_line(panel, math.ceil(width-1)/2, height-1, width-1, 0)
     draw_line(panel, width-1, 0, 0, 0)
-    display(panel)
+    display_panel(panel)
 
 
 if __name__ == "__main__":
+    panel = get_panel(10)
+    display_panel(panel)
     ()
